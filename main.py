7c92e4915dacff0fa04e843396c2b8c4b6ed75a3
Hugh Kelley <hkelley@tooledesign.com>
2020-09-03 12:53:09 -0400

    basic libs

---
+++ main.py
@@ -0,0 +1,8 @@
+# https://docs.python.org/3.5/library/smtplib.html
+import smtplib
+
+# https://python-twitter.readthedocs.io/en/latest/index.html
+import twitter
+
+# https://pypi.org/project/TikTokApi/
+import TikTokApi
