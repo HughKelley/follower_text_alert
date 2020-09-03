1b286946b6ab0fccd0fbc86595700ededfab5778
Hugh Kelley <hkelley@tooledesign.com>
2020-09-03 12:54:16 -0400

    psuedo code

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ import twitter
 
 # https://pypi.org/project/TikTokApi/
 import TikTokApi
+
+
+# load list of twitter followers
+
+# load list of tiktok followers
+
+# Compare to existing
+
+# if there's a difference built alert strings
+
+# connect to email server
+
+# send email
+
+
+
+
