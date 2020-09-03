f3693a7629a45ef76267d1097d88e28548cebbb4
Hugh Kelley <hkelley@tooledesign.com>
2020-09-03 12:59:25 -0400

    user inputs

--- main.py
+++ main.py
@@ -8,15 +8,29 @@ import twitter
 import TikTokApi
 
 
+
+
 # load list of twitter followers
 
+tw_username = input('enter twitter username')
+tw_password = input('enter twitter passwrod')
+
+tw_target = input("enter twitter handle of the person you'd like to check")
+
 # load list of tiktok followers
+
+tt_username = input('enter tiktok username')
+tt_password = input('inter tiktok password')
+tt_target = input("enter ticktok handle of the person you'd like to check")
 
 # Compare to existing
 
 # if there's a difference built alert strings
 
 # connect to email server
+
+gm_username = input("enter gmail username")
+gm_password = input("enter gmail password")
 
 # send email
 
